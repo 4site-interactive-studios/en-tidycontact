fc4081ae21f5b545ef88009248358e0dae560c34
Fernando Santos <fern@ndo.io>
2022-02-09 17:01:58 -0300

    First Working Version

--- README.md
+++ README.md
@@ -1,10 +1,76 @@
-# address-standardization-engaging-networks
+# Address Standardization Script for Engaging Networks
 
-Address Standardization for your Engaging Networks Forms
+This project integrates your Engaging Networks form with the Address Standardization service.
 
-# Install
+## How to use
+
+1. Add the script below to the Engaging Networks page:
+
+```html
+<script
+  defer="defer"
+  src="{YOUR_EN_URL}/as-en.js"
+  data-cid="{YOUR_AS_ID}"
+></script>
+```
+
+2. The script will look for the address fields following the Engaging Networks default field names. You need to provide a valid **cid** (Address Standardization Client ID) as a data attribute. You can also customize every field via data attributes.
+
+## Options
+
+Every option can be set as a data attribute on the `script` tag.
+
+- **cid** - Address Standardization Client ID (**required**).
+- **as_record** - Address Standardization Record. Set the field name to store the Address Standardization Record Data. Usually a hidden field.
+- **as_date** - Address Standardization Date. Set a field name to store the date. Every time the Address Standardization service is called, this field will be updated.
+- **as_status** - Address Standardization Status. Set a field name to store the status of the last Address Standardization attempt.
+- **address1** - Address 1 Field Name.
+- **address2** - Address 2 Field Name.
+- **city** - City Field Name.
+- **region** - State Field Name.
+- **postalCode** - Postal Code (zipcode) Field Name.
+- **country** - Country Field Name.
+
+<div style="page-break-after: always;"></div>
+
+## Default Options
+
+This is an example of the `script` tag with the default options:
+
+```html
+<script
+  defer="defer"
+  src="{YOUR_EN_URL}/as-en.js"
+  data-cid="0"
+  data-as_record=""
+  data-as_date=""
+  data-as_status=""
+  data-address1="supporter.address1"
+  data-address2="supporter.address2"
+  data-city="supporter.city"
+  data-region="supporter.region"
+  data-postalCode="supporter.postcode"
+  data-country="supporter.country"
+></script>
+```
+
+### IMPORTANT: This project only works on Engaging Networks Forms. The `window.enOnSubmit` event listener is used to change the fields.
+
+## Development
+
+Your js code must be on the `src/app` folder.
+Right now, this project has no styling changes, but the structure is ready for you to add your own on `src/scss`.
+
+## Install Dependencies
 
 1. `npm install`
-2. To generate the assets for production, run `npm run build`
-3. To start the DevServer with HOT Reload, use `npm run start`
-4. Build your project on the src folder.
+
+## Deploy
+
+1. `npm run build` - Builds the project
+2. `npm run watch` - Watch for changes and rebuilds the project
+
+It will create a `dist` folder, where you can get the `as-en.js` file and publish it.
+
+Currently it's published for tests on:  
+https://aaf1a18515da0e792f78-c27fdabe952dfc357fe25ebf5c8897ee.ssl.cf5.rackcdn.com/1874/as-en.js
--- src/app/utils/as-en.js
+++ src/app/utils/as-en.js
@@ -1,5 +1,22 @@
 export class asEN {
+  endpoint = "https://mid.4sitestudios.com/address-standardize/"; // Address Standardization API
+  wasCalled = false; // True if the API endpoint was called
+  isDirty = false; // True if the address was changed by the user
+  cid = 0; // Client ID
+  as_record = ""; // Address Standardization Record
+  as_date = ""; // Date of Address Standardization
+  as_status = ""; // Status of Address Standardization
+  fields = {
+    address1: "supporter.address1", // Address Field 1
+    address2: "supporter.address2", // Address Field 2
+    city: "supporter.city", // City field
+    region: "supporter.region", // State field
+    postalCode: "supporter.postcode", // Zipcode field
+    country: "supporter.country", // Country field
+  }; // Address Standardization Fields
+
   constructor() {
+    console.log("asEN constructor", this.shouldRun());
     if (this.shouldRun()) {
       if (document.readyState !== "loading") {
         this.init();
@@ -12,7 +29,8 @@ export class asEN {
     }
   }
   init() {
-    console.log("asEN init");
+    this.loadOptions();
+    this.addEventListeners();
   }
   shouldRun() {
     // Only run if there's a engaging networks form
@@ -21,5 +39,125 @@ export class asEN {
       return true;
     }
     return false;
+  }
+  loadOptions() {
+    // Load options from script tag
+    const scriptTag = document.querySelector("script[src*='as-en.js']");
+    this.fields.address1 =
+      scriptTag.getAttribute("data-address1") || this.fields.address1;
+    this.fields.address2 =
+      scriptTag.getAttribute("data-address2") || this.fields.address2;
+    this.fields.city = scriptTag.getAttribute("data-city") || this.fields.city;
+    this.fields.region =
+      scriptTag.getAttribute("data-region") || this.fields.region;
+    this.fields.postalCode =
+      scriptTag.getAttribute("data-postalCode") || this.fields.postalCode;
+    this.fields.country =
+      scriptTag.getAttribute("data-country") || this.fields.country;
+    this.as_record = scriptTag.getAttribute("data-as_record") || this.as_record;
+    this.as_date = scriptTag.getAttribute("data-as_date") || this.as_date;
+    this.as_status = scriptTag.getAttribute("data-as_status") || this.as_status;
+    this.cid = scriptTag.getAttribute("data-cid") || this.cid;
+  }
+  getField(name) {
+    // Get the field by name
+    return document.querySelector(`[name="${name}"]`);
+  }
+  getFieldValue(name) {
+    return new FormData(document.querySelector("form.en__component"))
+      .getAll(name)
+      .join(",");
+  }
+  addEventListeners() {
+    // Add event listeners to fields
+    for (const key in this.fields) {
+      const field = this.getField(this.fields[key]);
+      if (field) {
+        field.addEventListener("change", () => {
+          this.isDirty = true;
+        });
+      }
+    }
+    // Add event listener to submit
+    window.enOnSubmit = () => {
+      if (this.isDirty && !this.wasCalled) {
+        return this.callAPI();
+      }
+      return true;
+    };
+  }
+  callAPI() {
+    // Call the API
+    const address1 = this.getFieldValue(this.fields.address1);
+    const address2 = this.getFieldValue(this.fields.address2);
+    const city = this.getFieldValue(this.fields.city);
+    const region = this.getFieldValue(this.fields.region);
+    const postalCode = this.getFieldValue(this.fields.postalCode);
+    const country = this.getFieldValue(this.fields.country);
+    const data = {
+      address1,
+      address2,
+      city,
+      region,
+      postalCode,
+      country,
+      cid: this.cid,
+    };
+    this.wasCalled = true;
+    const ret = fetch(this.endpoint, {
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(new Date(), "callAPI response", data);
+        const recordField = this.getField(this.as_record);
+        const dateField = this.getField(this.as_date);
+        const statusField = this.getField(this.as_status);
+        if ("changed" in data && data.valid === true) {
+          let record = this.setFields(data.changed);
+          record["requestId"] = data.requestId;
+          if (recordField) {
+            recordField.value = JSON.stringify(record);
+          }
+          if (dateField) {
+            dateField.value = new Date().toLocaleString("en-US", {
+              hour12: false,
+            });
+          }
+          if (statusField) {
+            statusField.value = "Success";
+          }
+        } else if ("error" in data) {
+          if (recordField) {
+            recordField.value = JSON.stringify({ requestId: data.requestId });
+          }
+          if (dateField) {
+            dateField.value = new Date().toLocaleString("en-US", {
+              hour12: false,
+            });
+          }
+          if (statusField) {
+            statusField.value = data.error;
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("Address Standardization Error", error);
+      });
+    return ret;
+  }
+  setFields(data) {
+    let response = {};
+    // Set the fields
+    for (const key in data) {
+      const field = this.getField(this.fields[key]);
+      if (field) {
+        response[key] = { from: field.value, to: data[key] };
+        field.value = data[key];
+      }
+    }
+    return response;
   }
 }
